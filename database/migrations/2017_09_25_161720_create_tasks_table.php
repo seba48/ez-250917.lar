f7addf9f34dd45eb212114d8d00b902242b7b793
sasha44 <12345@i.ua>
2017-09-25 19:43:25 +0300

    craete migrations

--- database/migrations/2017_09_25_161720_create_tasks_table.php
+++ database/migrations/2017_09_25_161720_create_tasks_table.php
@@ -3,18 +3,17 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration {
-
+class CreateTasksTable extends Migration
+{
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up() {
-        Schema::create('users', function (Blueprint $table) {
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->unsigned()->index();
-            $table->string('name');
             $table->timestamps();
         });
     }
@@ -24,8 +23,8 @@ class CreateUsersTable extends Migration {
      *
      * @return void
      */
-    public function down() {
-        Schema::drop('users');
+    public function down()
+    {
+        Schema::drop('tasks');
     }
-
 }
