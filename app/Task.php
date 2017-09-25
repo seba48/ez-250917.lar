ca3b8ddd81e5f10a0b472d4c0eb31a3c00cb4eca
sasha44 <12345@i.ua>
2017-09-25 20:02:01 +0300

    relation Task-user

--- app/Task.php
+++ app/Task.php
@@ -4,12 +4,20 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Task extends Model
-{
+class Task extends Model {
+
     /**
-   * Массово присваиваемые атрибуты.
-   *
-   * @var array
-   */
-  protected $fillable = ['name'];
+     * Массово присваиваемые атрибуты.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+
+    /**
+     * Получить пользователя - владельца данной задачи
+     */
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
 }
