720d717f27bf38b8d44ab8bb641a5d8d054a7996
sasha44 <12345@i.ua>
2017-09-25 20:20:47 +0300

    auth TasksController task rout

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+
+class TaskController extends Controller {
+
+    /**
+     * Создание нового экземпляра контроллера.
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
+}
